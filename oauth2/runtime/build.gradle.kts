2ed0b4961d3115541afe424c59b60773964dc909
Alexandre Dutra <adutra@users.noreply.github.com>
2025-05-22 23:42:44 +0200

    generate empty javadoc and source jars

--- oauth2/runtime/build.gradle.kts
+++ oauth2/runtime/build.gradle.kts
@@ -22,8 +22,8 @@ plugins {
 
 dependencies { implementation(project(":authmgr-oauth2-core")) }
 
-// all dependencies are expected to be provided by Iceberg runtime jars, except the ones
-// that are explicitly declared in the dependencies block above
+// don't bring in any transitive dependencies; instead, include only
+// the dependencies that are not already in the Iceberg runtime jars
 configurations.all { isTransitive = false }
 
 tasks.shadowJar {
@@ -33,13 +33,10 @@ tasks.shadowJar {
   relocate("com.github.benmanes", "org.apache.iceberg.shaded.com.github.benmanes")
 }
 
-// configure the javadoc task to use the source and classpath of project ":authmgr-oauth2-core"
-tasks.withType<Javadoc> {
-  source = project(":authmgr-oauth2-core").tasks.named<Javadoc>("javadoc").get().source
-  classpath = project(":authmgr-oauth2-core").tasks.named<Javadoc>("javadoc").get().classpath
+tasks.named<Javadoc>("javadoc") {
+  // Generate empty javadoc
 }
 
-// configure the source jar to contain the sources of project ":authmgr-oauth2-core"
 tasks.named<Jar>("sourcesJar") {
-  from(project(":authmgr-oauth2-core").sourceSets["main"].allSource)
+  // Generate empty sources jar
 }
